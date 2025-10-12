5cf212c6755e2bacea86dcacfa562eec09aa6075
sumanyaselvi-r <145419732+sumanyaselvi-r@users.noreply.github.com>
2025-10-12 16:37:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <a href="https://www.linkedin.com/in/sumanya-selvi-r-2b8513274/" target="_blank" rel="noreferrer" aria-label="LinkedIn">
                 <FaLinkedin />
               </a>
-              <a href="https://github.com/https://github.com/sumanyaselvi-r" target="_blank" rel="noreferrer" aria-label="GitHub">
+              <a href="https://github.com/sumanyaselvi-r" target="_blank" rel="noreferrer" aria-label="GitHub">
                 <FaGithub />
               </a>
       </div>
